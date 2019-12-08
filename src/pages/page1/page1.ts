b103c92ca8d530594dea563ca0f50418616f9a16
Robson <rbsonvca@hotmail.com>
2019-12-08 17:03:25 -0300

    Implementa o as especificaçoes do exec1

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { stringify } from '@angular/core/src/util';
 
 @IonicPage()
 @Component({
@@ -7,9 +8,18 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'page1.html'
 })
 export class Page1 {
-
+  nome;
   constructor(public navCtrl: NavController) {
-
   }
 
+  troca() {
+    var input = document.getElementById('editNome');
+    var label = document.getElementById('labelMensagem');
+    if(typeof(this.nome) !== "undefined") {
+      if (this.nome.length >= 1){
+        label.innerText = "Alô, " + this.nome + "!";
+        this.nome = "";
+      } 
+    }
+  } 
 }
